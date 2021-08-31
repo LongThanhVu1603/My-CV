b1a1cef1084bcb5cbf70910ebc306776411837fe
Long Thanh Vu <Longthanhvu1603hn2@gmail.com>
2021-08-31 21:40:21 +0700

    lần 2

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-    $('#btn1').click(function(){
-        $('.text').load('index.html')
-    });
-});
+/*==================== SHOW MENU ====================*/
+
+const showMenu = (toggLeId, navId) =>{
+    const toggle = document.getElementById(toggLeId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
